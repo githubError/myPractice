f040e91be869c0801a7706e0eeef93e84891dca7
cuipengfei <cpf9401@163.com>
2016-05-21 21:11:41 +0000

    实现录音和文字输入间响应切换

--- 即时通信/CPFToolView.h
+++ 即时通信/CPFToolView.h
@@ -8,11 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    CPFToolViewRecordStart,
+    CPFToolViewRecordStop,
+    CPFToolViewRecordCancel
+}CPFToolViewRecordType;
+
+typedef void(^CPFToolViewMoreBtnBlock)();
+// 发送文字
+typedef void(^CPFToolViewTextFieldSendBlock)(UITextView *);
+// 开始编辑
+typedef void(^CPFToolViewTextFieldBeginEditBlock)(UITextView *);
+
+@protocol CPFToolViewRecordDelegate <NSObject>
+
+- (void)toolViewRecord:(UIButton *)recordBtn withType:(CPFToolViewRecordType)type;
+
+@end
+
 @interface CPFToolView : UIView <UITextViewDelegate>
 
 @property (nonatomic, strong) UIButton *recordBtn;
 @property (nonatomic, strong) UITextView *inputView;
 @property (nonatomic, strong) UIButton *moreSelectBtn;
 @property (nonatomic, strong) UIButton *sendTalkBtn;
+
+@property (nonatomic,assign)id<CPFToolViewRecordDelegate> delegate;
+@property (nonatomic, copy)CPFToolViewTextFieldSendBlock textFieldSendBlock;
+
+@property (nonatomic, copy)CPFToolViewTextFieldBeginEditBlock beginEditBlock;
+
+@property (nonatomic,copy)CPFToolViewMoreBtnBlock moreBtnBlock;
 
 @end
--- 即时通信/CPFToolView.m
+++ 即时通信/CPFToolView.m
@@ -8,6 +8,15 @@
 
 #import "CPFToolView.h"
 #import "UIViewExt.h"
+#import "TKAlertCenter.h"
+
+@interface CPFToolView ()
+{
+    __weak UIButton *weakRecordBtn;
+    __weak UITextView *weakInputView;
+}
+
+@end
 
 @implementation CPFToolView 
 
@@ -21,13 +30,15 @@
         _recordBtn.frame = CGRectMake(0, 0, 40, 40);
         [_recordBtn setBackgroundImage:[UIImage imageNamed:@"ToolViewInputVoice"] forState:UIControlStateNormal];
         [_recordBtn setBackgroundImage:[UIImage imageNamed:@"ToolViewInputVoiceHL"] forState:UIControlStateHighlighted];
+        [_recordBtn addTarget:self action:@selector(showRecordBtn) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_recordBtn];
         
-        _inputView = [[UITextView alloc] initWithFrame:CGRectMake(_recordBtn.right, 0, 240, 40)];
+        _inputView = [[UITextView alloc] initWithFrame:CGRectMake(_recordBtn.right, 5, 240, 30)];
         [_inputView setBackgroundColor:[UIColor whiteColor]];
         _inputView.layer.cornerRadius = 6.0;
         _inputView.layer.borderWidth = 1.0;
         _inputView.layer.borderColor = [UIColor whiteColor].CGColor;
+        _inputView.delegate = self;
         _inputView.returnKeyType = UIReturnKeySend;
         [self addSubview:_inputView];
         
@@ -36,12 +47,72 @@
         [_moreSelectBtn setBackgroundImage:[UIImage imageNamed:@"TypeSelectorBtn_Black"] forState:UIControlStateNormal];
         [_moreSelectBtn setBackgroundImage:[UIImage imageNamed:@"TypeSelectorBtnHL_Black"] forState:UIControlStateHighlighted];
         [self addSubview:_moreSelectBtn];
+        
+        _sendTalkBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _sendTalkBtn.frame = CGRectMake(_recordBtn.right, 5, 240, 30);
+        [_sendTalkBtn setTitle:@"按住 说话" forState:UIControlStateNormal];
+        
+        _sendTalkBtn.backgroundColor = [UIColor whiteColor];
+        _sendTalkBtn.layer.cornerRadius = 6.0;
+        
+        [_sendTalkBtn setTitle:@"松手 发送" forState:UIControlStateHighlighted];
+        [_sendTalkBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_sendTalkBtn setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+        [_sendTalkBtn addTarget:self action:@selector(start:) forControlEvents:UIControlEventTouchDown];
+        [_sendTalkBtn addTarget:self action:@selector(stop:) forControlEvents:UIControlEventTouchUpInside];
+        [_sendTalkBtn addTarget:self action:@selector(cancel:) forControlEvents:UIControlEventTouchUpOutside];
+        [_sendTalkBtn setHidden:YES];
+        [self addSubview:_sendTalkBtn];
     }
     return self;
 }
 
+// 显示录音按钮
+- (void)showRecordBtn {
+    static BOOL isShowRecod = YES;
+    if (isShowRecod) {
+        _sendTalkBtn.hidden = NO;
+        isShowRecod = NO;
+    }else {
+        _sendTalkBtn.hidden = YES;
+        isShowRecod = YES;
+    }
+}
+
+// 开始录音
+- (void)start:(UIButton *)btn
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(toolViewRecord:withType:)]) {
+        [self.delegate toolViewRecord:btn withType:CPFToolViewRecordStart];
+    }
+    [[TKAlertCenter defaultCenter] postAlertWithMessage:@"开始录音"];
+}
+
+// 结束录音
+- (void)stop:(UIButton *)btn
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(toolViewRecord:withType:)]) {
+        [self.delegate toolViewRecord:btn withType:CPFToolViewRecordStop];
+    }
+}
+
+// 退出录音
+- (void)cancel:(UIButton *)btn
+{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(toolViewRecord:withType:)]) {
+        [self.delegate toolViewRecord:btn withType:CPFToolViewRecordCancel];
+    }
+    [[TKAlertCenter defaultCenter] postAlertWithMessage:@"录音已取消"];
+}
+
+
+#pragma mark - UITextViewDelegate
+// 动态改变输入框文本高度
 - (void)textViewDidChange:(UITextView *)textView {
     _inputView.height = textView.contentSize.height;
     self.height = _inputView.height;
+    if ([textView.text hasSuffix:@"\n"]) {
+        [textView resignFirstResponder];
+    }
 }
 @end
