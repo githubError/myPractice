99a516bcb0cfd60751096ae81869200bf66bfaf2
cuipengfei <cpf9401@163.com>
2016-05-26 13:26:01 +0000

    优化CPFMeViewController

--- 即时通信/即时通信/CPFMeViewController.h
+++ 即时通信/即时通信/CPFMeViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CPFMeViewController : UITableViewController
+@interface CPFMeViewController : UIViewController
 
 @end
--- 即时通信/即时通信/CPFMeViewController.m
+++ 即时通信/即时通信/CPFMeViewController.m
@@ -12,8 +12,17 @@
 #import "MBProgressHUD+Add.h"
 #import "EaseMob.h"
 #import "TKAlertCenter.h"
+#import "CPFButton.h"
 
-@interface CPFMeViewController ()
+#define kScreenWidth UIScreen.mainScreen.bounds.size.width
+#define kScreenHeight UIScreen.mainScreen.bounds.size.height
+
+@interface CPFMeViewController ()<UITableViewDataSource, UITableViewDelegate>
+{
+    UITableView *_tableView;
+    CPFButton *_logoutBtn;
+}
+
 
 @end
 
@@ -22,7 +31,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"reuseIdentifier"];
+    
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight - 200) style:UITableViewStylePlain];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"reuseIdentifier"];
+    
+    _logoutBtn = [CPFButton shareButton];
+    _logoutBtn.frame = CGRectMake(self.view.center.x - 50, kScreenHeight - 180, 100, 30);
+    
+    UIImage *BtnBkgImg = [UIImage imageNamed:@"logout_click"];
+    BtnBkgImg = [BtnBkgImg stretchableImageWithLeftCapWidth:BtnBkgImg.size.width*0.5 topCapHeight:BtnBkgImg.size.height*0.5];
+    UIImage *BtnBkgImgHL = [UIImage imageNamed:@"logout"];
+    BtnBkgImgHL = [BtnBkgImgHL stretchableImageWithLeftCapWidth:BtnBkgImgHL.size.width*0.5 topCapHeight:BtnBkgImgHL.size.height*0.5];
+    
+    [_logoutBtn setBackgroundImage:BtnBkgImg forState:UIControlStateNormal];
+    [_logoutBtn setBackgroundImage:BtnBkgImgHL forState:UIControlStateHighlighted];
+    [_logoutBtn setTitle:@"退出登录" forState:UIControlStateNormal];
+    [_logoutBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_logoutBtn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    
+    __block CPFMeViewController *meViewCtr = self;
+    _logoutBtn.clickBlock = ^(CPFButton *btn){
+        [MBProgressHUD showMessag:@"正在退出" toView:meViewCtr.view];
+        [[EaseMob sharedInstance].chatManager asyncLogoffWithUnbindDeviceToken:YES completion:^(NSDictionary *info, EMError *error) {
+            if (!error) {
+                NSLog(@"======%@",info);
+                AppDelegate *myDelegate = [UIApplication sharedApplication].delegate;
+                [myDelegate isLogoffSuccess];
+                [MBProgressHUD hideAllHUDsForView:meViewCtr.view animated:YES];
+            } else {
+                [[TKAlertCenter defaultCenter] postAlertWithMessage:@"退出失败"];
+                [MBProgressHUD hideAllHUDsForView:meViewCtr.view animated:YES];
+            }
+        } onQueue:nil];
+    };
+    
+    [self.view addSubview:_logoutBtn];
+    
+    [self.view addSubview:_tableView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,17 +87,13 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return 2;
+    return 1;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
     
-    if (indexPath.row == 1) {
-        cell.textLabel.text = @"退出登录";
-        [cell.textLabel setTextColor:[UIColor redColor]];
-    }
     if (indexPath.row == 0) {
         cell.textLabel.text = [[EaseMob sharedInstance].chatManager loginInfo][@"username"];
         cell.imageView.image = [UIImage imageNamed:@"default_header"];
@@ -59,20 +103,7 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.row == 1) {
-        [MBProgressHUD showMessag:@"正在退出" toView:self.view];
-        [[EaseMob sharedInstance].chatManager asyncLogoffWithUnbindDeviceToken:YES completion:^(NSDictionary *info, EMError *error) {
-            if (!error) {
-                NSLog(@"======%@",info);
-                AppDelegate *myDelegate = [UIApplication sharedApplication].delegate;
-                [myDelegate isLogoffSuccess];
-                [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
-            } else {
-                [[TKAlertCenter defaultCenter] postAlertWithMessage:@"退出失败"];
-                [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
-            }
-        } onQueue:nil];
-    }
+    
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -80,6 +111,13 @@
         return 80;
     }
     return 50;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return 1;
+    }
+    return 30;
 }
 
 @end
